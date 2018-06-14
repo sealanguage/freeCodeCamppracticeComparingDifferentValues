bdd381b0792b03b1efd2cb8b70f5850342779d51
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-06-14 20:26:04 +0000

    initial commit

---
+++ practiceComparingDifferentValues.js
@@ -0,0 +1,9 @@
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
